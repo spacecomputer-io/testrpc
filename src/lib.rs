0b534890e3c7732a8d760e248fb87e34bb33c8e2
amirylm <83904651+amirylm@users.noreply.github.com>
2025-02-28 12:51:34 +0200

    added e2e test

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,64 @@ pub mod hotshot;
 pub mod jrpc;
 pub mod runner;
 pub mod signal;
+
+#[cfg(test)]
+mod test {
+    use std::{env, sync::Arc};
+
+    use super::*;
+    use tokio::time::{sleep, Duration};
+
+    #[tokio::test]
+    async fn test_e2e_dry_run() {
+        env::set_var("DRY_RUN", "true"); // run in dry-run mode
+        let raw_cfg_yaml: &str = r#"
+interval: 1
+iterations: 4
+num_of_nodes: 4
+adapter: hotshot
+args: # arguments for the adapter
+  coordinator_url: http://127.0.0.1:3030
+round_templates:
+  10_txs:
+    txs: 10
+    tx_size: 100
+rounds:
+  - rpcs: [3,0]
+    use_template: 10_txs
+  - rpcs: [1,2]
+    template:
+        txs: 10
+        tx_size: 1000
+        "#; 
+        let cfg = config::parse_config_yaml(raw_cfg_yaml).unwrap();
+        let rpc_urls = vec![
+            "http://localhost:5000".to_string(),
+            "http://localhost:5001".to_string(),
+            "http://localhost:5002".to_string(),
+            "http://localhost:5003".to_string(),
+        ];
+        let ctx = Arc::new(ctx::Context::new());
+        let ctx_cloned = ctx.clone();
+        let handle = tokio::spawn(async move {
+            // wait for the test to complete
+            // use a timeout of <interval> * <interations> + buffer
+            let buffer = 1;
+            let timeout = 1 * 4 + buffer;
+            sleep(Duration::from_secs(timeout)).await;
+            ctx_cloned.stop();
+        });
+        tokio::select! {
+            _ = handle => {
+                panic!("Timed out w/o completion");
+            }
+            Ok(results) = runner::run(ctx, cfg, rpc_urls) => {
+                assert_eq!(results.len(), 4);
+                for result in results {
+                    assert_eq!(result.sent, 20);
+                    assert_eq!(result.failed, 0);
+                }
+            }
+        };
+    }
+}
